6e09b30f7b103d95fab5af77797fe886aa3dd7f6
rackstiel <rickstoel@hotmail.com>
2025-09-17 19:45:18 +0200

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -1,4 +1,6 @@
 import streamlit as st
 import pandas as pd
 import gspread
+from google.oauth2.service_account import Credentials
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 st.write("gspread import werkt!")
